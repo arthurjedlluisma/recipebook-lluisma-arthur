474bf7619389ebfbd36cd512d20d804259dd6c86
Arthur Lluisma <arthur.lluisma@student.ateneo.edu>
2024-03-04 23:12:41 +0800

    Setup django admin

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Recipe, RecipeIngredient
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    inlines = [
+        RecipeIngredientInline,
+    ]
+
+
+admin.site.register(Recipe, RecipeIngredientAdmin)
