16768aec87558949d5a81e814cca0820971935fe
Arthur Lluisma <arthur.lluisma@student.ateneo.edu>
2024-03-04 22:32:43 +0800

    Edit urls in ledger/urls.py and html files

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from .views import recipe_one, recipe_two, recipes_list
+from .views import RecipeDetailView, RecipeListView
 
 urlpatterns = [
-    path("recipes/list", recipes_list, name="recipes_list"),
-    path("recipe/1", recipe_one, name="recipe_one"),
-    path("recipe/2", recipe_two, name="recipe_two"),
+    path("recipes/list", RecipeListView.as_view(), name="recipes-list"),
+    path("recipe/<int:pk>", RecipeDetailView.as_view(), name="recipe-detail"),
 ]
 
 app_name = "ledger"
